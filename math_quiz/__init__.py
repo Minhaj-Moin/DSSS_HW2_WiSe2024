9e5c507472e2a7be604203bf8e093532286f42d5
Minhaj-Moin <minhaj.moin@outlook.com>
2024-11-09 07:34:41 +0100

    Packaged

---
+++ math_quiz/__init__.py
@@ -0,0 +1,7 @@
+"""
+Math Quiz
+"""
+
+__version__ = "0.0.1"
+__author__ = 'Minhaj Ahmed Moin'
+__credits__ = 'Minhaj Ahmed Moin'
---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='math_quiz',
+    version='0.0.1',    
+    description='A Math Quiz Package',
+    url='https://github.com/Minhaj-Moin/DSSS_HW2_WiSe2024.git',
+    author='Minhaj Ahmed Moin',
+    author_email='minhaj.moin@fau.de',
+    license='Apache',
+    packages=[],
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Academia',
+        'License :: OSI Approved :: Apache',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
